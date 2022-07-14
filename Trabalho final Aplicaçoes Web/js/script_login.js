c7b88820390d6dcb74757d6117f1bf5ee2807231
Eric Posser <101907800+ericposser@users.noreply.github.com>
2022-07-14 19:15:19 -0300

    Add files via upload

---
+++ Trabalho final Aplicaçoes Web/js/script_login.js
@@ -0,0 +1,11 @@
+function logar(){
+    var login = document.getElementById("idemail").value;
+    var senha = document.getElementById("idsenha").value;
+
+    if(login == "eric" && "eric"){
+        alert("bem-vindo");
+    }
+    else{
+        alert("usuario ou senha incorretos");
+    }
+}
